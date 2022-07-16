45b2d6fa38128f005c579770fe5c907d5e8f34b7
indrakuu <kurniawan.indra0727@gmail.com>
2022-07-17 01:10:18 +0700

    penambahan favicon

--- resources/views/backend/include/app.blade.php
+++ resources/views/backend/include/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') | Multi Role User - DuniaDev</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}">
     
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" type="text/css">
     <link
